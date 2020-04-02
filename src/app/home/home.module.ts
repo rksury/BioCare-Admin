66e43979811c73d6d35f04a84b3d109f9ab566ba
devloper <rksury@gmail.com>
2020-04-02 20:53:05 +0530

    update data table

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -9,6 +9,20 @@ import { DoctorComponent } from './doctor/doctor.component';
 import { ChemistComponent } from './chemist/chemist.component';
 import { LabComponent } from './lab/lab.component';
 import { PatientComponent } from './patient/patient.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -26,6 +40,21 @@ import { PatientComponent } from './patient/patient.component';
     HomeRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSortModule,
+    MatTableModule
 
   ]
 })
