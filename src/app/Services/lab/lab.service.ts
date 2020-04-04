0baca9b216761c88affc46fd3a5c22f33e1b05ca
ayush3298 <abairagi311@gmail.com>
2020-04-05 03:32:31 +0530

    added Lab crud

--- src/app/Services/lab/lab.service.ts
+++ src/app/Services/lab/lab.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {GodService} from '../god.service';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,27 @@ export class LabService {
 
   getLabs(params?) {
     return this.godService.authGet('chemist/');
+  }
+
+  getLab(pk): Observable<any> {
+    return this.godService.authGet('chemist/' + pk);
+  }
+
+  ApproveLab(pk) {
+    const data = {id: pk}
+    return this.godService.authPost('chemist/approve', data);
+  }
+
+  addLab(data) {
+    return this.godService.authPost('chemist/', data);
+  }
+
+  deleteLab(pk) {
+    return this.godService.authDelete('chemist/' + pk);
+  }
+
+  updateLab(pk, data) {
+    return this.godService.authUpdate('chemist/' + pk, data);
+
   }
 }
