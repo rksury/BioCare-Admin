f402d8c97310e62e85234c3bd4435e58bd4e5ffc
ayush3298 <abairagi311@gmail.com>
2020-04-05 03:14:36 +0530

    added chemist crud

--- src/app/Services/chemist/chemist.service.ts
+++ src/app/Services/chemist/chemist.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {GodService} from '../god.service';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,25 @@ export class ChemistService {
   constructor(private godService: GodService) {
   }
 
+
+  getChemist(pk): Observable<any> {
+    return this.godService.authGet('chemist/' + pk);
+  }
+
   getChemists(params?) {
     return this.godService.authGet('chemist/');
+  }
+
+  addChemist(data) {
+    return this.godService.authPost('chemist/', data);
+  }
+
+  deleteChemist(pk) {
+    return this.godService.authDelete('chemist/' + pk);
+  }
+
+  updateChemist(pk, data) {
+    return this.godService.authUpdate('chemist/' + pk, data);
+
   }
 }
--- src/app/home/chemist/chemist.component.ts
+++ src/app/home/chemist/chemist.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ChemistService} from '../../Services/chemist/chemist.service';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-chemist',
@@ -9,8 +10,23 @@ import {ChemistService} from '../../Services/chemist/chemist.service';
 export class ChemistComponent implements OnInit {
   chemists;
   show = false;
+  chemistToEdit;
   dtOptions: DataTables.Settings = {};
 
+  ChemistForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    email: new FormControl(''),
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    mobile_number: new FormControl(''),
+    address1: new FormControl(''),
+    address2: new FormControl(''),
+    zip_code: new FormControl(''),
+    city: new FormControl(''),
+    country: new FormControl(''),
+  });
 
   constructor(private chemistService: ChemistService) {
   }
@@ -24,6 +40,40 @@ export class ChemistComponent implements OnInit {
       this.chemists = chemists;
       this.show = true;
     });
+  }
+
+  addChemist() {
+    this.chemistService.addChemist(this.ChemistForm.value).subscribe();
+  }
+
+  deleteChemist(pk) {
+    this.chemistService.deleteChemist(pk).subscribe();
+  }
+
+
+  editChemistForm(pk) {
+    this.chemistService.getChemist(pk).subscribe(doctor => {
+      this.ChemistForm.reset();
+      this.ChemistForm.patchValue({
+        username: doctor.user.username,
+        email: doctor.user.email,
+        first_name: doctor.user.first_name,
+        last_name: doctor.user.last_name,
+        mobile_number: doctor.user.mobile_number,
+        address1: doctor.user.address.address1,
+        address2: doctor.user.address.address2,
+        zip_code: doctor.user.address.zip_code,
+        city: doctor.user.address.city,
+        country: doctor.user.address.country,
+      });
+      this.chemistToEdit = doctor.id;
+    });
+  }
+
+  editChemist() {
+    this.chemistService.updateChemist(this.chemistToEdit, this.ChemistForm.value).subscribe();
+    this.chemistToEdit = null;
+    this.ChemistForm.reset();
   }
 
 }
